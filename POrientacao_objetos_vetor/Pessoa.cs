e2cd4ab0e377e5aa8fcc19780c08b915a856238e
Filipe Anjos <110917838+Anjosfilipe@users.noreply.github.com>
2022-08-23 20:29:42 -0300

    Adicionar arquivos de projeto.

---
+++ POrientacao_objetos_vetor/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POrientacao_objetos_vetor
+{
+    internal class Pessoa
+    {
+        String nome;
+        int data_nascimento;
+        double cpf, tel;
+
+
+        public Pessoa(string nome, int data_nascimento, double cpf, double tel) // método construtor  
+        {
+            this.nome = nome;
+            this.data_nascimento = data_nascimento;
+            this.tel = tel;
+            this.cpf = cpf;
+
+        }
+
+
+        public void setNome(String nome) //método público para inserção de valores
+        {
+            this.nome = nome;   // inserção de idade pelo usuario 
+        }
+        public void setIdade(int data_nascimento) // método público para inserção de valores 
+        {
+            this.data_nascimento = data_nascimento; // inserção de data de nascimento pelo usuario 
+        }
+
+        public void settel(double tel) // método público para inserção de valores 
+        {
+            this.tel = tel; // inserção do telefone pelo usuario 
+        }
+
+        public void setcpf(double cpf) // método público para inserção de valores 
+        {
+            this.cpf = cpf; // inserção do cpf pelo usuario 
+        }
+
+        public override String ToString() // override - escrever por cima 
+        {
+            return "\n\nNome: " + nome + "\nData de Nascimento: " + data_nascimento + "\nTelefone: " + tel + "\nCPF: " + cpf;
+
+        }
+    }
+}
---
+++ POrientacao_objetos_vetor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace POrientacao_objetos_vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            int data_nascimento;
+            double cpf, tel;
+
+            Pessoa[] pessoa = new Pessoa[3];
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("\nDigite seu Nome: ");
+                nome = Console.ReadLine();
+
+                Console.WriteLine("\nDigite a data de nascimento: ");
+                data_nascimento = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite seu CPF: ");
+                cpf = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite seu Telefone: ");
+                tel = double.Parse(Console.ReadLine());
+
+                pessoa[i] = new Pessoa(nome, data_nascimento, cpf, tel);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(pessoa[i].ToString());
+
+            }
+
+
+        }
+    }
+}
